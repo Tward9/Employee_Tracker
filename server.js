6edb09440695250463a15a8508ec58183f6352a5
Theo Ward <theoward9@gmail.com>
2022-04-18 15:46:52 -0500

    draft inquirer, set up tables in schema

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mysql = require('mysql2');
 // Import Inquirer
 const inquirer = require('inquirer');
+// Import Table Package
+const cTable = require('console.table');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,13 +17,65 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password
-    password: 'rootPassword',
-    database: ''
-  },
-  console.log(`Connected to the ___ database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password
+        password: 'rootPassword',
+        database: ''
+    },
+    console.log(`Connected to the ___ database.`)
 );
+
+const questions = [
+    {
+        type: 'list',
+        name: 'doWhat',
+        message: 'What would you like to do?',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Finish'],
+    }
+];
+
+function manageBuisness(){
+    inquirer.prompt(questions).then((answers) => {
+        if (answers.doWhat === 'View all Departments') {
+            //something
+
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'View all Roles') {
+            //something
+            
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'View all Employees') {
+            //something
+
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'Add a Department') {
+            //something
+
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'Add a Role') {
+            //something
+
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'Add an Employee') {
+            //something
+            
+            //then
+            manageBuisness();
+        }else if (answers.doWhat === 'Update an Employee Role') {
+            //something
+
+            //then
+            manageBuisness();
+        }else {
+            //Stop program
+        };
+    });
+};
