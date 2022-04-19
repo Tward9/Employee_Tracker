f32a71cc6a978c58f01c18d7988637130457b9c8
Theo Ward <theoward9@gmail.com>
2022-04-19 18:11:05 -0500

    adding roles, employees, and departments all work

--- server.js
+++ server.js
@@ -34,54 +34,146 @@ const questions = [
         name: 'doWhat',
         message: 'What would you like to do?',
         choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Finish'],
-    }
+    },
+    {
+        type: 'input',
+        name: 'addDepartment',
+        message: 'What is the name of the New Department',
+        when(answers) {
+            return answers.doWhat === 'Add a Department'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addRoleName',
+        message: 'What is the Name of the new Role',
+        when(answers) {
+            return answers.doWhat === 'Add a Role'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addRoleSalary',
+        message: 'What is the Salary of the new Role',
+        when(answers) {
+            return answers.doWhat === 'Add a Role'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addRoleDepartment',
+        message: 'What is the Department of the new Role',
+        when(answers) {
+            return answers.doWhat === 'Add a Role'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addEmployeeFname',
+        message: 'What is the First Name of the new Employee',
+        when(answers) {
+            return answers.doWhat === 'Add an Employee'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addEmployeeLname',
+        message: 'What is the Last Name of the new Employee',
+        when(answers) {
+            return answers.doWhat === 'Add an Employee'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addEmployeeRole',
+        message: 'What is the Role of the new Employee',
+        when(answers) {
+            return answers.doWhat === 'Add an Employee'
+        },
+    },
+    {
+        type: 'input',
+        name: 'addEmployeeManager',
+        message: 'Who is the Manager of the new Employee, select null for no manager',
+        default: null,
+        when(answers) {
+            return answers.doWhat === 'Add an Employee'
+        },
+    },
 ];
 
-function manageBuisness(){
+function manageBuisness() {
     inquirer.prompt(questions).then((answers) => {
         if (answers.doWhat === 'View all Departments') {
             //query to see table of departments
             db.query('SELECT * FROM department', function (err, results) {
+                console.log('');
                 console.table(results);
-              });
+            });
             //then
             manageBuisness();
-        }else if (answers.doWhat === 'View all Roles') {
+        } else if (answers.doWhat === 'View all Roles') {
             //query to see table of roles
             db.query('SELECT * FROM role', function (err, results) {
+                console.log('');
                 console.table(results);
-              });
+            });
             //then
             manageBuisness();
-        }else if (answers.doWhat === 'View all Employees') {
+        } else if (answers.doWhat === 'View all Employees') {
             //query to see table of employees
             db.query('SELECT * FROM employee', function (err, results) {
+                console.log('');
                 console.table(results);
-              });
+            });
             //then
             manageBuisness();
-        }else if (answers.doWhat === 'Add a Department') {
+        } else if (answers.doWhat === 'Add a Department') {
+            //something
+            db.query(`INSERT INTO department (department_name) VALUES ('${answers.addDepartment}')`, function (err, results) {
+                console.log('');
+                console.log('Adding New Department');
+            });
+            db.query('SELECT * FROM department', function (err, results) {
+                console.log('');
+                console.table(results);
+            });
+            //then
+            manageBuisness();
+        } else if (answers.doWhat === 'Add a Role') {
+            //something
+            db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${answers.addRoleName}', ${answers.addRoleSalary}, ${answers.addRoleDepartment})`, function (err, results) {
+                console.log('');
+                console.log('Adding New Department');
+            });
+            db.query('SELECT * FROM role', function (err, results) {
+                console.log('');
+                console.table(results);
+            });
+            //then
+            manageBuisness();
+        } else if (answers.doWhat === 'Add an Employee') {
+            //something
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answers.addEmployeeFname}', '${answers.addEmployeeLname}', ${answers.addEmployeeRole}, ${answers.addEmployeeManager})`, function (err, results) {
+                console.log('');
+                console.log('Adding New Department');
+            });
+            db.query('SELECT * FROM employee', function (err, results) {
+                console.log('');
+                console.table(results);
+            });
+            //then
+            manageBuisness();
+        } else if (answers.doWhat === 'Update an Employee Role') {
             //something
 
             //then
             manageBuisness();
-        }else if (answers.doWhat === 'Add a Role') {
-            //something
-
-            //then
-            manageBuisness();
-        }else if (answers.doWhat === 'Add an Employee') {
-            //something
-            
-            //then
-            manageBuisness();
-        }else if (answers.doWhat === 'Update an Employee Role') {
-            //something
-
-            //then
-            manageBuisness();
-        }else {
+        }
+        else {
             //Stop program
+            console.log('Edit Complete');
+            process.exit(1);
         };
     });
 };
